de9fd0d2faf872b0143a16f29babbe0d0f108f36
kiptykDima <kiptykdmytro@gmail.com>
2023-02-09 23:12:46 +0200

    second commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+
+
+flsFunctions.openBurgerMenu();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
